163a02b8756312e26abfd2b9fba95415bd682b0a
marcel <marcel.maciaszek@icloud.com>
2023-05-22 15:53:58 +0200

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Marcel Maciaszek on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
